c9075bd3a76f4acc71575e8b6a6786750fa791ff
RyanG0101 <139116461+RyanG0101@users.noreply.github.com>
2023-08-28 15:34:52 +0530

    BinomialCoefficientusingDP.cpp

--- BinomialCoefficientusingDP.cpp
+++ BinomialCoefficientusingDP.cpp
@@ -3,26 +3,35 @@
 using namespace std;
 
 
+int min(int a, int b);
 int binomialCoeff(int n, int k)
 {
-	//base
-	if (k > n)
-		return 0;
-	if (k == 0 || k == n)
-		return 1;
+	int C[n + 1][k + 1];
+	int i, j;
 
-	// Recur
-	return binomialCoeff(n - 1, k - 1)
-		+ binomialCoeff(n - 1, k);
+	for (i = 0; i <= n; i++) {
+		for (j = 0; j <= min(i, k); j++) {
+			// Base Cases
+			if (j == 0 || j == i)
+				C[i][j] = 1;
+
+		
+			else
+				C[i][j] = C[i - 1][j - 1] + C[i - 1][j];
+		}
+	}
+
+	return C[n][k];
 }
 
-/* Driver code*/
+
+int min(int a, int b) { return (a < b) ? a : b; }
+
+// Driver Code
 int main()
 {
 	int n = 5, k = 2;
-	cout << "Value of C(" << n << ", " << k << ") is "
+	cout << "Value of C[" << n << "][" << k << "] is "
 		<< binomialCoeff(n, k);
-	return 0;
 }
-
 
